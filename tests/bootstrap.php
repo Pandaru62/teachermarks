b872c122908c32b7f1b3bc4218e22c7ea75f35fc
Loris Pandaru <loris.buchelet@gmail.com>
2024-07-14 15:42:16 +0200

    test2

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,8 +4,9 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
 
-if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
-    require dirname(__DIR__).'/config/bootstrap.php';
-} elseif (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+if (file_exists(dirname(__DIR__).'/.env')) {
+    (new Dotenv())->loadEnv(dirname(__DIR__).'/.env');
+} else {
+    // Load from environment variables
+    (new Dotenv())->populate($_SERVER);
 }
